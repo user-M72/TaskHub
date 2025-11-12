9bb87f2bc795db0d3c7f63573e6b989924c86195
LinKord Azamov <muzaffarazamov007@gmail.com>
2025-11-12 20:58:56 +0300

    Begin

---
+++ src/main/java/App/TaskHub/TaskHubApplication.java
@@ -0,0 +1,13 @@
+package App.TaskHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskHubApplication.class, args);
+	}
+
+}
