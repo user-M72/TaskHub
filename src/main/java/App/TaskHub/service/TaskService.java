8831759071977619c80fe6887e394a39dcc295aa
LinKord Azamov <muzaffarazamov007@gmail.com>
2026-02-02 16:09:48 +0500

    added new method for statuses

--- src/main/java/App/TaskHub/service/TaskService.java
+++ src/main/java/App/TaskHub/service/TaskService.java
@@ -2,6 +2,9 @@ package App.TaskHub.service;
 
 import App.TaskHub.dto.req.task.TaskRequest;
 import App.TaskHub.dto.res.task.TaskResponse;
+import App.TaskHub.entity.Task;
+import App.TaskHub.entity.enums.TaskPriority;
+import App.TaskHub.entity.enums.TaskStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -18,4 +21,8 @@ public interface TaskService {
     TaskResponse updated(UUID id, TaskRequest request);
 
     void deleted(UUID id);
+
+    Task updateStatus(UUID id, TaskStatus status);
+
+    Task updatePriority(UUID id, TaskPriority priority);
 }
