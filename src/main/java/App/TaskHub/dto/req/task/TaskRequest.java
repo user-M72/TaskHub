1b4884c3f770fed5424daf7da97ec9d98857367c
LinKord Azamov <muzaffarazamov007@gmail.com>
2026-02-09 09:50:27 +0500

    Added blank for TaskRequest

--- src/main/java/App/TaskHub/dto/req/task/TaskRequest.java
+++ src/main/java/App/TaskHub/dto/req/task/TaskRequest.java
@@ -2,21 +2,20 @@ package App.TaskHub.dto.req.task;
 
 import App.TaskHub.entity.enums.TaskPriority;
 import App.TaskHub.entity.enums.TaskStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record TaskRequest(
-
-        String title,
+        @NotBlank String title,
         String description,
         TaskStatus status,
-        TaskPriority priority,
+        @NotNull TaskPriority priority,
         String project,
-        LocalDateTime dueDate,
-        UUID assigneeId,
-        UUID creatorId
-
-
+        @NotNull LocalDateTime dueDate,
+        @NotNull UUID assigneeId,
+        @NotNull UUID creatorId
 ){}
 
